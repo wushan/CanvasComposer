3f0cfb7ef6fa0a31e8412e9128292b4075a9c1e5
wushan <chiangloveslynn@gmail.com>
2016-04-19 16:30:21 +0800

    not using babel anymore

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -35,7 +35,6 @@ gulp.task('scripts', () => {
   return gulp.src('app/scripts/**/*.js')
     .pipe($.plumber())
     .pipe($.sourcemaps.init())
-    .pipe($.babel())
     .pipe($.sourcemaps.write('.'))
     .pipe(gulp.dest('.tmp/scripts'))
     .pipe(reload({stream: true}));
